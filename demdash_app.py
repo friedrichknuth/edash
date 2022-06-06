b693b6383c2beefaaea224c9b4feaabdd8646bdd
Seth Vanderwilt <sethv@github.com>
2022-06-05 23:01:22 -0700

    demo version of app with some links included

--- demdash_app.py
+++ demdash_app.py
@@ -5,7 +5,8 @@ import seaborn as sns
 
 import numpy as np
 import geopandas as gpd
-import xarray as xr
+
+from matplotlib_scalebar.scalebar import ScaleBar
 
 
 import leafmap.foliumap as leafmap
@@ -21,9 +22,10 @@ from demdash_functions import *
 st.set_page_config(layout="wide")
 st.title("DEM Dashboard Tool")
 
-years = [2014,2015,2016,2017,2018,2019,2020,2021]
-dems = {}
+years = [2015,2016,2017,2018,2019,2020,2021]
 
+#years = [2014,2015,2016,2017,2018,2019,2020,2021]
+dems = {}
 dem_filenames = {}
 for year in years:
     dem_filenames[year] = f"test_data_dir/{year}.tif"
@@ -31,30 +33,41 @@ for year in years:
     dems[year] = rioxarray.open_rasterio(f"test_data_dir/{year}.tif", masked=True)
 
 
+with st.sidebar:
+    tool_choice = st.radio("Choose tool", ("DEM Difference", "DEM Explorer", "Timelapse"))
+    # st.write("Current tool:", add_radio)
 
 # with col1:
-st.header("Difference map tool")
-st.text("Adjust the sliders to choose which start and end year to compute elevation difference map")
+if tool_choice == "DEM Difference":
+    st.header("Elevation difference map: choose start and end year using sliders")
 
-values = st.select_slider("Year", options=years, value=(years[0], years[-1]))
+    values = st.select_slider("Year", options=years, value=(years[0], years[-1]))
 
-fig,ax = plt.subplots(1,1)
-diff = dems[values[1]] - dems[values[0]]
-diff.plot(ax=ax)
+    fig,ax = plt.subplots(1,1, figsize=(8,5))
+    diff = dems[values[1]] - dems[values[0]]
+    absmax = max(abs(diff.min()), abs(diff.max()))
+    plt.imshow(diff.transpose("y","x","band").to_numpy(), rasterized=True, cmap="RdBu", vmin=-absmax, vmax=absmax)
+    plt.colorbar(label="End Z - Start Z (meters)")
+    # diff.plot(ax=ax)
+    scale = diff.rio.resolution()[0]
+    ax.add_artist(ScaleBar(scale, fixed_value=100))
+    plt.xlabel("X (meters)")
+    plt.ylabel("Y (meters)")
 
-st.pyplot(fig)
 
+    st.pyplot(fig)
+    st.write("Difference map goes here, turned off while working on other features")
 
 
 ### TRANSECT TOOL DISPLAY
-st.header("Transect tool: for now draw a line between any two points")   
-st.text("Export as GeoJSON, drag file onto uploader, our code will sample the points along your line and plot the elevation profile")
-geojson_file = st.file_uploader("Upload GeoJSON")
+st.header("Transect tool: pick a point, draw a line, or sketch a polygon. Export then drag GeoJSON onto uploader to see elevation/slope/aspect statistics")
+st.markdown("Demo workaround: hillshades/color images aren't rendering in the gray box when viewing remotely, clue=[Github](https://github.com/streamlit/streamlit/pull/4677)")
+uploaded_geojson_file = st.file_uploader("Upload GeoJSON")
 
 col1, col2 = st.columns([10,6])
 
 
-m = leafmap.Map(latlon_control=False, draw_export=True, zoom=12)#, crs="EPSG:32610")
+m = leafmap.Map(google_map="HYBRID", draw_export=True)#latlon_control=True, draw_export=True)#, zoom=13)#, crs="EPSG:32610")
 # m.add_cog_layer("/mnt/1.0_TB_VOLUME/sethv/cse512/a3/edash/dem_raster_stack/2021_cog.tif")
 # m.add_local_tile("/mnt/1.0_TB_VOLUME/sethv/cse512/a3/edash/dem_raster_stack/2021.tif", layer_name="dem2021")
 # diff.rio.to_raster("diff.tif")
@@ -63,6 +76,15 @@ m = leafmap.Map(latlon_control=False, draw_export=True, zoom=12)#, crs="EPSG:326
 #     m.add_local_tile(dem_filenames[year], layer_name=f"{year}")
 
 # m.add_local_tile("diff.tif", layer_name="diff_from_above", cmap="RdBu")
+# m.split_map(left_layer="OpenTopoMap", right_layer="OpenTopoMap")
+
+# TODO trying to figure out how to get the TIFFs to show up when viewing from other machines
+# https://github.com/streamlit/streamlit/pull/4677
+
+# url = 'https://opendata.digitalglobe.com/events/california-fire-2020/pre-event/2018-02-16/pine-gulch-fire20/1030010076004E00.tif'
+# url = "http://localhost:39881/2021_hs_cog.tif"
+# m.add_cog_layer("http://localhost:40000")#, name="2021_hs_cog.tif")
+# m.add_local_tile("test_data_dir/resampled_sethv1_easton_20210924_without_gcps_transparent_mosaic_group1.tif") #  http://127.0.0.1:39189 is where it looked for tile
 m.add_local_tile("test_data_dir/2021_hs.tif", layer_name="hs2021")#, colormap="terrain")#, debug=True)
 # m.add_local_tile("/mnt/1.0_TB_VOLUME/sethv/cse512/a3/edash/dem_raster_stack/2021_hs.tif", layer_name="hs2021")#, colormap="terrain")#, debug=True)
 
@@ -77,113 +99,161 @@ with open(gj_ex_filename) as gj_ex_file:
     gdf_epsg4326_ex = gpd.read_file(gj_ex_filename)
     gdf_ex = gdf_epsg4326_ex.to_crs("epsg:32610")
 
-m.add_geojson(gj_ex, layer_name="Example GeoJSON transect used to make plots", style={"color":"blue", "fillColor":"blue"})
+if uploaded_geojson_file is not None:
+    geojson_file = uploaded_geojson_file
+    gj = geojson.loads(geojson_file.getvalue())
+else:
+    # use example geojson to show how to use the tool
+    geojson_file = gj_ex_filename
+    with open(geojson_file) as f:
+        gj = geojson.load(f)#s(geojson_file.getvalue())
+
+# load the geojson ONCE and figure out which kind of geometry we are dealing with
+
+gdf_epsg4326 = gpd.read_file(geojson_file)
+gdf = gdf_epsg4326.to_crs("epsg:32610")
+assert len(gdf.geometry) == 1, "We only support GeoJSONs with exactly 1 polygon/line/point"
+
 
 with col2:
-    fig,ax = plt.subplots(1,1,figsize=(6,3))
-    fig.suptitle("EXAMPLE transect already on map elevation profile")
-    plot_line_elevation(gdf=gdf_ex, ax=ax, dems=dems, years=years, name="name of transect goes here", color="blue")
+
+    # Make 3 subplots of elevation, slope, aspect regardless of user's GeoJSON type
+    fig,ax = plt.subplots(3,1,figsize=(6,9))
+    
+    feature_type = gdf.geometry.type[0]
+    if feature_type == "LineString":
+        # TODO where should this sort of color scheme setting happen?
+        sns.set_palette(sns.color_palette("tab10"))
+
+        sns.set_palette("Blues", n_colors=10)
+        # fig.suptitle("profile of user uploaded transect")
+        plot_line_elevation(gdf=gdf, ax=ax[0], dems=dems, years=years, title="Elevation profile along transect")
+        plot_line_slope(gdf=gdf, ax=ax[1], dems=dems, years=years, title="Slopes along transect")
+        plot_line_aspect(gdf=gdf, ax=ax[2], dems=dems, years=years, title="Distribution of aspects along transect")
+        plt.tight_layout()
+
+    elif feature_type == "Point":
+        # sns.set_palette(sns.color_palette("Blues", as_cmap=True).reversed(), n_colors=1)
+        # fig.suptitle("Time series at this point")
+        plot_point_elevation(gdf=gdf, ax=ax[0], dems=dems, years=years, title="Elevation time series @ point")
+        plot_point_slope(gdf=gdf, ax=ax[1], dems=dems, years=years, title="Slope time series @ point")
+        plot_point_aspect(gdf=gdf, ax=ax[2], dems=dems, years=years, title="Aspect time series @ point")
+        plt.tight_layout()
+
+    elif feature_type == "Polygon":
+        # sns.set_palette("Blues", n_colors=1)
+        fig.suptitle("elevation/slope/aspect stats for this polygon")
+        plot_polygon_elevation(gdf=gdf, ax=ax[0], dems=dems, years=years, title="Elevation within polygon")
+        plot_polygon_slope(gdf=gdf, ax=ax[1], dems=dems, years=years, title="Slope within polygon")
+        plot_polygon_aspect(gdf=gdf, ax=ax[2], dems=dems, years=years, title="Aspect within polygon")
+        plt.tight_layout()
+    
+    else:
+        raise NotImplementedError("Unsupported GeoJSON type")
+
     st.pyplot(fig)
 
+# Draw the user's transect/point/polygon on the map
+m.add_geojson(gj, layer_name="JSON profile uploaded by user", style={"color":"red", "fillColor":"red"})
 
-    fig,ax = plt.subplots(1,1,figsize=(6,3))
-    dss = get_profile(gdf_ex, dems[2021])[1]
-    # ax.hist(dss.data.squeeze(), color="blue")
-    ax.set_title("KDEPLOT of EXAMPLE elevation values")
-    seaborn_hist = sns.kdeplot(dss.data.squeeze(), ax=ax)
-    st.pyplot(fig)
+# TODO quick hack for demo to work around invisible rasters...
+# Since rasters are not rendering on remote machines accessing the app,
+# instead just rely on the basemap and draw a box around the DEM extent
+# Generated raster extent with gdaltindex in EPSG:4326
+with open("test_data_dir/dems_box_epsg4326.geojson") as f:
+    gj_raster_extent = geojson.load(f)
 
-if geojson_file is not None: 
-    #TODO load the geojson ONCE and figure out which kind of geometry we are dealing with
-    # if this is a box:
-
-    gdf_epsg4326 = gpd.read_file(geojson_file)
-    gdf = gdf_epsg4326.to_crs("epsg:32610")
-    assert len(gdf.geometry) == 1, "We assume GeoJSON with only 1 polygon/line/point in them"
-
-    with col2:
-        st.write(type(gdf.geometry.type[0]))
-        feature_type = gdf.geometry.type[0]
-        if feature_type == "LineString":
-            fig,ax = plt.subplots(3,1,figsize=(6,3))
-            fig.suptitle("profile of user uploaded transect")
-            plot_line_elevation(gdf=gdf, ax=ax[0], dems=dems, years=years, name="name of transect goes here", color="blue")
-            # SLOPE
-            # ASPECT
-
-            st.pyplot(fig)
-
-        if feature_type == "Point":
-            fig,ax = plt.subplots(3,1,figsize=(6,3))
-            fig.suptitle("timeseries at this point")
-            # plot_point_elevation()
-            
-
-    
-    """
-    get_average_elevation_time_series(gdf)
-    
-    fig,ax = plt.subplots(1,1,figsize=(6,3))
-    print(geojson_file.name)
-    """
-
-    gj = geojson.loads(geojson_file.getvalue())
-
-    m.add_geojson(gj, layer_name="JSON profile uploaded by user, could be 1 of n ?", style={"color":"red", "fillColor":"red"})
-    # st.text(f"Plot of profile w/ '{geojson_file.name}' goes here, this is printed when the geojson file is loaded")
-    # st.write("Not actually going to print the GEOJSON just want to see coordinates below before doing profile extraction")
-    # st.write(gj)
-
-    
-    
-    # Sample from the DEM raster
-
-    
-    
- 
-    # Instead of df try dss.hvplot???
-#     profile = dss.plot.line(ax=ax,
-#                                    # x="distance",
-#                                    y="band",
-#                                                  # title='elevation',
-#                                                  # xlabel='Distance (m)',
-#                                                  # ylabel='Elevation (m)',
-#                                                  # width=300, height=150, 
-#                                                  # shared_axes=False
-#                           )
-
-    
-    #df = get_profile(point_stream, num=10)
-#     profile = df.hvplot.line(y='elevation').opts(ax=ax,
-#                                                  title='elevation',
-#                                                  xlabel='Distance (m)',
-#                                                  ylabel='Elevation (m)',
-#                                                  width=300, height=150, 
-#                                                  shared_axes=False)
-    
-    
-    # Show the actual values along the transect
-    
-    
-    # This should be a histogram of aspect but not going to compute that yet
-    # dems[2021].plot.hist(ax=ax)
-
-
+# Would have to Reproject to EPSG 4326 if not done in gdaltindex
+m.add_geojson(gj_raster_extent, layer_name="Extent of study area DEMs", style={"color":"gray", "fillColor":"none"})
 
 with col1:
+    # The map goes in the first column
     m.to_streamlit()
-
-
 
 st.header("Timelapse tool (apps will go on separate pages eventually)")
 st.text("Work in progress porting \"Create Timelapse\" tool from streamlit.geemap.org") 
 
+# # https://matplotlib.org/stable/gallery/animation/dynamic_image.html
+# # https://matplotlib.org/stable/api/_as_gen/matplotlib.animation.FuncAnimation.html
+# import matplotlib.animation as animation
+# from matplotlib_scalebar.scalebar import ScaleBar
+# # import matplotlib.pyplot as plt
+# # fig,ax=plt.subplots()
 
+# # def func(frame, *args):
+# #     print(args)
+# #     assert 1==0
 
+# # animation.FuncAnimation(fig, func, frames=[1,2,3,4])
+# # ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True,
+# #                                 repeat_delay=1000)
 
+# # https://stackoverflow.com/questions/34975972/how-can-i-make-a-video-from-array-of-images-in-matplotlib
+# # img = [] # some array of images
+# frames = [] # for storing the generated images
+# fig,ax = plt.subplots(ncols=2, figsize=[20,10])
+# # plt.suptitle("Easton Glacier Surface Elevation (Hillshade)", fontsize=48)
+# ax[0].set_xticks([])
+# ax[0].set_yticks([])
+# ax[1].set_xticks([])
+# ax[1].set_yticks([])
+# titles = {
+#     2014: "2014 UAV SfM",
+#     2015: "2015 Lidar",
+#     2016: "2016 UAV SfM",
+#     2017: "2017 UAV SfM",
+#     2018: "2018 UAV SfM",
+#     2019: "2019 UAV SfM",
+#     2020: "2020 UAV SfM",
+#     2021: "2021 UAV SfM"
+# }
+# plt.rcParams['animation.embed_limit'] = 30000000 # 20971520.0 limit
 
+# for year in range(2015,2022):
+#     # year = 2014+i # TODO hacky
+    
+#     # orthos[3]
+    
+#     scale = 0.5
+#     frames.append([
+#         ax[0].set_title("Easton Glacier Surface Elevation (Hillshade)", fontsize=24),
+        
+#         ax[0].imshow(dems[year].squeeze(), cmap="gray", 
+#             # clim=pltlib.get_clim(hs),
+#             rasterized=True, animated=True),
+#         # ax[0].imshow(dems[i], alpha=0.6, clim=[1550,1850], rasterized=True, animated=True),
+#         ax[0].text(250, 100, f"{titles[year]}", ha='center', va='center', fontsize=24, backgroundcolor="White", color="Black"),
+        
+#         # ax[1].set_title("Easton Glacier Orthoimage", fontsize=24),
+        
+#         # # Gross but have to do an exceptional plot of 2015 lidar intensity
+#         # ax[1].imshow(orthos[i].transpose("y","x","band"), rasterized=True, animated=True) if orthos[i].shape[0] > 1 else \
+#         #     ax[1].imshow(orthos[i].squeeze(), cmap="binary", vmin=0, vmax=20000, rasterized=True, animated=True),
+        
+        
+#         # ax[1].add_artist(ScaleBar(scale, fixed_value=100)),
+#         # ax[1].text(250, 100, f"{titles[year]}", ha='center', va='center', fontsize=24, backgroundcolor="White", color="Black")
 
-## OTHER USEFUL CODE BELOW THIS LINE
+#     ])
+        
+# ani = animation.ArtistAnimation(fig, frames, interval=50, blit=True,
+#                                 repeat_delay=1000)
+
+# from IPython.display import HTML
+# animation = HTML(ani.to_jshtml())
+# # ani.save('movie.mp4')
+# # plt.show()
+# # https://github.com/egagli/sar_snow_melt_timing/blob/main/backscatter_visualization_and_gifs.ipynb
+# hillshade_timseries_animation_fn = "easton_hillshade_ortho_timeseries_animation_2014-2021.gif"
+# ani.save(hillshade_timseries_animation_fn, writer="pillow", fps=1)
+# # ani.save("movie.mp4") # no ffmpeg access via jupyter
+# # st.write(ani.to_html5_video())
+# st.markdown(ani.to_jshtml(), unsafe_allow_html=True)
+# st.image(hillshade_timseries_animation_fn, output_format="GIF")
+# st.pyplot(ani) # didn't work
+# animation
+
+## OTHER PREVIOUSLY USEFUL CODE BELOW THIS LINE
 
 
 
--- demdash_functions.py
+++ demdash_functions.py
@@ -2,83 +2,229 @@ import streamlit as st
 
 import rioxarray
 import matplotlib.pyplot as plt
+import seaborn as sns
+import rasterio
 
 import numpy as np
 import geopandas as gpd
 import xarray as xr
+from osgeo import gdal
 
 
 ### TRANSECT TOOL FUNCTIONS
 
 # POINT
-def plot_point_elevation():
-    raise NotImplementedError
-def plot_point_slope():
-    raise NotImplementedError
-def plot_point_aspect():
-    raise NotImplementedError
-
-# BOX
-
-#def get_polygon_elevation():
-def get_average_elevation_time_series(geojson_filename, color="red"):
-    gf_epsg4326 = gpd.read_file(geojson_filename)
-    gf = gf_epsg4326.to_crs("epsg:32610")
-    sgeom = gf.geometry.values[0]
-    means = []
+def plot_point_elevation(gdf, ax, dems, years, title="Elevation time series @ point"):
+    """Plot time series of DEM values at this point"""
+    # TODO prefer to use time series stack
+    assert len(gdf.geometry) == 1, "only expecting 1 point"
+    values = []
     for year in years:
-        clipped = dems[2021].rio.clip(gf.geometry)
-        means.append(clipped.mean().squeeze())
+        value = dems[year].sel(x=gdf.geometry.x[0], y=gdf.geometry.y[0], method="nearest")
+        values.append(value)
 
-    fig,ax= plt.subplots(1,1,figsize=(6,3))
-    ax.scatter(years, means, color=color)
-    plt.legend()
-    st.pyplot(fig)
-    
-def plot_polygon_elevation():
+    ax.plot(years, values)
+    ax.set_ylabel("Elevation (meters)")
+    ax.set_xlabel("Year")
+    ax.ticklabel_format(useOffset=False)
+    # ax.legend()
+    ax.set_title(title)
+
+def plot_point_slope(gdf, ax, dems, years, title="Slope time series @ point"):
+    assert len(gdf.geometry) == 1, "only expecting 1 point"
+    values = []
+    for year in years:
+        dem = dems[year]
+        slope = compute_slope(dem)
+        value = slope.sel(x=gdf.geometry.x[0], y=gdf.geometry.y[0], method="nearest")
+        values.append(value)
+
+    ax.plot(years, values)
+    ax.set_ylabel("Slope (degrees)")
+    ax.set_xlabel("Year")
+    # ax.legend()
+    ax.set_title(title)
+
+def plot_point_aspect(gdf, ax, dems, years, title="Aspect time series @ point"):
+    assert len(gdf.geometry) == 1, "only expecting 1 point"
+    values = []
+    for year in years:
+        dem = dems[year]
+        aspect = compute_aspect(dem)
+        value = aspect.sel(x=gdf.geometry.x[0], y=gdf.geometry.y[0], method="nearest")
+        values.append(value)
+
+    ax.plot(years, values)
+    ax.set_ylabel("Aspect (degrees)")
+    ax.set_xlabel("Year")
+    # ax.legend()
+    ax.set_title(title)
+
+# POLYGON
+
+def plot_polygon_elevation(gdf=None,ax=None, dems=None, years=None, title=None):
     # TODO this should use rasterstats
-    raise NotImplementedError
-def plot_polygon_slope():
-    raise NotImplementedError
-def plot_polygon_aspect():
-    raise NotImplementedError
+    # from rasterstats import zonal_stats
+    # value = zonal_stats(gdf.geometry, dem) # rasterstats broken
 
-# LINE
-def get_profile(gdf, dem):
-     #TODO this only supports  1 SEGMENT and 1 YEAR
-     # TODO point query thing support NaNs uw-cryo raster sampling library
-     # TODO interpolation of elevation profile
+    values = []
+    for year in years:
+        dem = dems[year]
+        region = dem.rio.clip(gdf.geometry)
+        value = {
+            "mean": region.mean(),
+            "std": region.std()
+        }
+        values.append(value)
 
-    # Reuse plotProfile_elev code
+    means = np.array([v["mean"] for v in values])
+    errors = np.array([v["std"] for v in values])
+    ax.plot(years, means)#, color="blue")
+    # ax.errorbar(years, means, yerr=errors)
+    ax.fill_between(years, means-errors, means+errors, color="gray", alpha=0.1)
+
+    ax.set_ylabel("Elevation (meters)")
+    ax.set_xlabel("Year")
+    # ax.legend()
+    ax.set_title(title)
+    
+def plot_polygon_slope(gdf=None,ax=None, dems=None, years=None, title=None):
+    values = []
+    for year in years:
+        dem = dems[year]
+        slope = compute_slope(dem)
+        region = slope.rio.clip(gdf.geometry)
+        value = {
+            "mean": region.mean(),
+            "std": region.std()
+        }
+        values.append(value)
+
+    means = np.array([v["mean"] for v in values])
+    errors = np.array([v["std"] for v in values])
+    ax.plot(years, means)
+    # ax.errorbar(years, [v["mean"] for v in values], yerr=[v["std"] for v in values])
+    ax.fill_between(years, means-errors, means+errors, color="gray", alpha=0.1)
+
+
+    # ax.legend()
+    ax.set_title(title)
+    ax.set_xlabel("Year")
+    ax.set_ylabel("Slope (degrees)")
+
+
+def plot_polygon_aspect(gdf=None,ax=None, dems=None, years=None, title=None):
+    values = []
+    for year in years:
+        dem = dems[year]
+        aspect = compute_aspect(dem)
+        region = aspect.rio.clip(gdf.geometry)
+        value = {
+            "mean": region.mean(),
+            "std": region.std()
+        }
+        values.append(value)
+
+        # st.write(region.shape)
+        sns.kdeplot(region.data.reshape(-1), ax=ax, label=str(year))
+    
+    # ax.plot(years, [v["mean"] for v in values])
+    # ax.errorbar(years, [v["mean"] for v in values], yerr=[v["std"] for v in values])
+
+    ax.legend()
+    ax.set_xlabel("Aspect (degrees)")
+    ax.set_title(title)
+
+# Reuse plotProfile_elev code
+def get_points_from_gdf(gdf, num=100):
     num = 100
-
-    
     sgeom = gdf.geometry.values[0]
-    
     distances = np.linspace(0, gdf.geometry.length.values[0], num) 
     points = [sgeom.interpolate(x) for x in distances]
     xs = xr.DataArray([p.x for p in points], dims='distance', coords=[distances])
     ys = xr.DataArray([p.y for p in points], dims='distance', coords=[distances])
+    return distances, xs, ys
+
+def sample_from_raster(raster, xs, ys):
+    """returns a ??? type array"""
+    # df = dss.to_dataframe(name='elevation')
+    return raster.sel(x=xs, y=ys, method='nearest')
+
+# LINE
+def get_profile(gdf, dem):
+    """Get elevation along a given transect from this DEM"""
+     #TODO this only supports  1 SEGMENT and 1 YEAR
+     # TODO point query thing support NaNs uw-cryo raster sampling library
+     # TODO interpolation of elevation profile
+    distances, xs, ys = get_points_from_gdf(gdf, num=100)
+
+    dss = sample_from_raster(dem, xs, ys)
     
-    # sample elevation
-    dss = dem.sel(x=xs, y=ys, method='nearest')
-    
-    # dss = ds['elevation'].sel(x=xs, y=ys, method='nearest')
-    df = dss.to_dataframe(name='elevation')
-    # print(dss)
-    
+    return distances, dss
+
+def compute_slope(dem):
+    # TODO check a3 tools
+    # Write to a new TIFF so that we can use GDAL
+    # TODO delete temporary rasters
+    # TODO BUG weird stuff with writing multiple versions of same raster filename
+    tmp_filename = f"tmp_slope_aspect_rasters/tmp{random.randint(1,10000)}.tif"
+    dem.rio.to_raster(tmp_filename)
+    tmp_out_filename = f"tmp_slope_aspect_rasters/tmp_out{random.randint(1,10000)}.tif"
+    gdal.DEMProcessing(tmp_out_filename, tmp_filename, 'slope')
+    return rioxarray.open_rasterio(tmp_out_filename, masked=True)
+
+
+import random
+def compute_aspect(dem):
+    # TODO check a3 tools
+    # Write to a new TIFF so that we can use GDAL
+    # TODO delete temporary rasters
+    tmp_filename = f"tmp_slope_aspect_rasters/tmp{random.randint(1,10000)}.tif"
+    dem.rio.to_raster(tmp_filename)
+    tmp_out_filename = f"tmp_slope_aspect_rasters/tmp_out{random.randint(1,10000)}.tif"
+    gdal.DEMProcessing(tmp_out_filename, tmp_filename, 'aspect')
+    return rioxarray.open_rasterio(tmp_out_filename, masked=True)
+
+def get_slope(gdf, dem):
+    distances, xs, ys = get_points_from_gdf(gdf, num=1000)
+    slope = compute_slope(dem)
+    dss = sample_from_raster(slope, xs, ys)
+    dss = dss.rolling(distance=10, center=True).mean()
+
+    return distances, dss
+
+def get_aspect(gdf, dem):
+    distances, xs, ys = get_points_from_gdf(gdf, num=100)
+    aspect = compute_aspect(dem)
+    dss = sample_from_raster(aspect, xs, ys)
     return distances, dss
     
 
-def plot_line_elevation(gdf=None,ax=None, dems=None, years=None, name=None, color="blue"):
-    colors = [] # TODO want a color graident instead of random colors
+def plot_line_elevation(gdf=None,ax=None, dems=None, years=None, title=None, color="blue"):
     for year in years:
         distances, dss = get_profile(gdf, dems[year])
         ax.plot(distances, dss.data.squeeze(), label=str(year))#, color=color)
-        plt.legend()
+        ax.legend()
+        ax.set_xlim([0,distances.max()])
 
-def plot_line_slope(gdf,dem):
-    raise NotImplementedError
+    ax.set_title(title)
+    ax.set_ylabel("Elevation (meters)")
+    ax.set_xlabel("Distance along transect (meters)")
+
+def plot_line_slope(gdf=None,ax=None, dems=None, years=None, title=None, color="blue"):
+    for year in years:
+        distances, dss = get_slope(gdf, dems[year])
+        ax.plot(distances, dss.data.squeeze(), label=str(year))#, color=color)
+        ax.set_xlim([0,distances.max()])
+        # ax.legend()
+    ax.set_title(title)
+    ax.set_xlabel("Distance along transect (meters)")
+    ax.set_ylabel("Slope (degrees)")
     
-def plot_line_aspect(gdf,dem):
-    raise NotImplementedError
+def plot_line_aspect(gdf=None,ax=None, dems=None, years=None, title=None):
+    for year in years:
+        distances, dss = get_aspect(gdf, dems[year])
+        sns.kdeplot(dss.data.squeeze(), ax=ax, label=str(year))
+        # ax.legend()
+    ax.set_title(title)
+    ax.set_xlabel("Aspect (degrees)")
